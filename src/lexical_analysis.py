6d9c3e869e88e804077d586b031f26dfd66500b3
Laboratorio XXIC - 103 <lab103c@lab.unesc.net>
2022-03-25 20:04:58 -0300

    Changed back to using a dictionary

--- src/lexical_analysis.py
+++ src/lexical_analysis.py
@@ -1,26 +1,65 @@
 from .utils import is_a_number
 
-tokens = [
-    "Program", "Label", "Const", "Var", "Procedure", "Begin", "End",
-    "Integer", "Array", "Of", "Call", "Goto", "If", "Then", "Else", "While",
-    "Do", "Repeat", "Until", "Readln", "Writeln", "Or", "And", "Not",
-    "Identificador", "Inteiro", "For", "To", "Case", "+", "-", "*", "/", "[",
-    "]", "(", ")", ":=", ":", "=", ">", ">=", "<", "<=", "<>", ",", ";",
-    "literal", ".", ".."
-]
+tokens = {
+    1: "Program",
+    2: "Label",
+    3: "Const",
+    4: "Var",
+    5: "Procedure",
+    6: "Begin",
+    7: "End",
+    8: "Interger",
+    9: "Array",
+    10: "Of",
+    11: "Call",
+    12: "Goto",
+    13: "If",
+    14: "Then",
+    15: "Else",
+    16: "While",
+    17: "Do",
+    18: "Repeat",
+    19: "Until",
+    20: "Readln",
+    21: "Writeln",
+    22: "Or",
+    23: "And",
+    24: "Not",
+    25: "Identificador",
+    26: "Inteiro",
+    27: "For",
+    28: "To",
+    29: "Case",
+    30: "+",
+    31: "-",
+    32: "*",
+    33: "/",
+    34: "[",
+    35: "]",
+    36: "(",
+    37: ")",
+    38: ":=",
+    39: ":",
+    40: "=",
+    41: ">",
+    42: ">=",
+    43: "<",
+    44: "<=",
+    45: "<>",
+    46: ",",
+    47: ";",
+    48: "literal",
+    49: ".",
+    50: "..",
+}
 
 
 def check_word(word):
-    if is_a_number(word):
-        return int(word), tokens.index("Inteiro")
-    else:
-        try:
-            idx = tokens.index(word)
-        except:
-            # TODO: implement verification for "Identificador"
-            idx = tokens.index("Identificador")
+    for key, value in tokens.items():
+        if value == word:
+            return key, value;
 
-        if idx == -1:
-            raise NameError("Invalid token!")
+    if (is_a_number(word)):
+        return 26, int(word)
 
-        return word, tokens[idx]
+    return 25, word
