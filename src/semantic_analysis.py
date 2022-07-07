5ed4a22d00f52db506ecf21ab30504964098246b
Arthur Fernandes <arthurbpfernandes@gmail.com>
2022-07-06 23:37:07 -0300

    feat: improve semantic analysis

--- src/semantic_analysis.py
+++ src/semantic_analysis.py
@@ -1,15 +1,17 @@
 import enum
 from src.tokens import Token
+from src.utils import is_a_number
 
 
 class SemanticSymbol:
 
-    def __init__(self, symbol, category, symbol_type, level, scope_name):
+    def __init__(self, symbol, category, symbol_type, level, scope_stack):
         self.symbol = symbol
         self.category = category
         self.symbol_type = symbol_type
         self.level = level
-        self.scope_name = scope_name
+        self.scope_name = scope_stack[-1]
+        self.scope_stack = scope_stack
 
 
 class Categories(enum.Enum):
@@ -19,6 +21,8 @@ class Categories(enum.Enum):
     var = 4
     procedure = 5
     begin = 6
+
+    parameter = -1
 
 
 def isIdentifier(token: Token):
@@ -45,11 +49,23 @@ def semantic_analysis(input_stack: list[Token]):
 
     symbols = []
 
-    def searchInTable(symbol):
+    def isDeclared(symbol: SemanticSymbol):
         for table_symbol in symbols:
             if (table_symbol.symbol.lower() == symbol.symbol.lower()
                     and table_symbol.level == symbol.level):
-                return table_symbol
+                return True
+        return False
+
+    def searchInTable(symbol: str, level: int, scope_stack: list[str]):
+        while level > 0:
+            for table_symbol in symbols:
+                if (table_symbol.symbol.lower() == symbol.lower()
+                        and table_symbol.level == level
+                        and table_symbol.scope_name == scope_stack[-1]):
+                    return table_symbol
+
+            level -= 1
+            scope_stack.pop(-1)
 
     for idx, token in enumerate(input_stack):
 
@@ -85,32 +101,95 @@ def semantic_analysis(input_stack: list[Token]):
                 start_of_scope = False
 
             symbol = SemanticSymbol(token.token, category, "", scope_level,
-                                    scope_stack[-1])
+                                    scope_stack.copy())
 
-            table_symbol = searchInTable(symbol)
             if (isDeclaration(category)):
                 # Checks if variable is being declared for the second time
-                if (table_symbol != None):
+                if (isDeclared(symbol)):
                     raise Exception(
                         f"Declaration not allowed. Line: {token.line}")
 
-                # TODO Check for procedure parameters
+                symbol_type = ""
 
+                # Checks for variable types
+                if (symbol.category == Categories.const.value):
+                    symbol_type = "Integer"
+
+                if (symbol.category == Categories.var.value):
+                    is_type_declaration = False
+
+                    for _token in input_stack[idx:]:
+                        # Token : detected, type declaration starts here
+                        if _token.ref_code == 39:
+                            is_type_declaration = True
+                            continue
+
+                        # Token ; detected, break out of loop
+                        if _token.ref_code == 47:
+                            break
+
+                        if is_type_declaration:
+                            # If keyword Of or Integer
+                            if _token.ref_code == 10 or (_token.ref_code == 8
+                                                         and
+                                                         symbol_type != ""):
+                                symbol_type = symbol_type + " "
+
+                            symbol_type = symbol_type + str(_token.token)
+
+                # Check for procedure parameters
+                if (symbol.category == Categories.procedure.value):
+                    if (symbols[-1].symbol == scope_stack[-1]):
+                        symbol_type = "Integer"
+                        symbol.category = Categories.parameter.value
+
+                symbol.symbol_type = symbol_type
                 symbols.append(symbol)
             else:
+                table_symbol = searchInTable(symbol.symbol, symbol.level,
+                                             symbol.scope_stack.copy())
+
                 # Checks if variable was not declared
                 if (table_symbol == None):
                     raise Exception(
                         f"Undeclared variable or procedure. Line: {token.line}"
                     )
 
-                # Checks if const is being assigned a new value
-                if (isAssignment()
-                        and table_symbol.category == Categories.const.value):
-                    raise Exception(
-                        f"Const assignment is not allowed. Line: {token.line}")
+                if (isAssignment()):
+                    # Checks if const is being assigned a new value
+                    if (table_symbol.category == Categories.const.value):
+                        raise Exception(
+                            f"Const assignment is not allowed. Line: {token.line}"
+                        )
 
-                    # TODO Check if assignment is compatible with variable type
+                    # Check if assignment is compatible with variable type
+                    for _token in input_stack[idx:]:
+                        # Token ; detected, break out of loop
+                        if _token.ref_code == 47:
+                            break
+
+                        if (is_a_number(_token.token) and table_symbol.symbol_type != 'Integer'):
+                            raise Exception(
+                                f"Invalid assignment. Line: {token.line}")
+
+                        if isIdentifier(_token):
+                            _symbol = SemanticSymbol(_token.token, category,
+                                                     "", scope_level,
+                                                     scope_stack.copy())
+
+                            _table_symbol = searchInTable(
+                                _symbol.symbol, _symbol.level,
+                                _symbol.scope_stack.copy())
+
+                            if (_table_symbol == None):
+                                break
+
+                            # TODO Check for array
+
+                            if (_table_symbol.symbol_type != table_symbol.symbol_type):
+                                raise Exception(f"Invalid assignment. Line: {token.line}")
+
+                    # TODO Check if array is being assigned within supported range
 
                 # TODO Check if procedure calls are being called with the correct arguments
 
